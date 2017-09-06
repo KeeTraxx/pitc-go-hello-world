1375b7e64180df136270ea3bd991ba4a51e6824f
Khôi Tran <tran@puzzle.ch>
2017-09-06 12:16:21 +0200

    Added comments

--- main.go
+++ main.go
@@ -8,6 +8,47 @@ import (
 	"github.com/mdp/qrterminal"
 )
 
+func main() {
+	// Variable assingment
+	// Note: type is implicitely set
+	helloworld := "Hello World!"
+
+	fmt.Println(helloworld)
+
+	// Open a file for read operations
+	file, err := os.OpenFile("participants.csv", os.O_RDONLY, os.ModePerm)
+
+	// Error handling, the Go idiomatic way
+	if err != nil {
+		panic(err)
+	}
+
+	// Declare a slice
+	var participants []Participant
+
+	gocsv.UnmarshalFile(file, &participants)
+
+	qrcode := ""
+
+	// loop through slice
+	for index, participant := range participants {
+		// Call a struct method
+		participant.PrettyPrint(index)
+
+		// Append to string
+		qrcode += fmt.Sprintln(participant.Name)
+	}
+
+	// Call a package method
+	printQRcode(qrcode)
+
+}
+
+// Non-exported package method
+func printQRcode(s string) {
+	qrterminal.Generate(s, qrterminal.L, os.Stdout)
+}
+
 // Participant represents a tech workshop participant
 type Participant struct {
 	Initials string `csv:"Initialen"`
@@ -16,32 +57,14 @@ type Participant struct {
 	Unit     string `csv:"Abteilung"`
 }
 
-func main() {
-	helloworld := "Hello World!"
-
-	fmt.Println(helloworld)
-
-	file, err := os.OpenFile("participants.csv", os.O_RDONLY, os.ModePerm)
-
-	if err != nil {
-		panic(err)
-	}
-
-	var participants []Participant
-
-	gocsv.UnmarshalFile(file, &participants)
-
-	qrcode := ""
-
-	for index, participant := range participants {
-		fmt.Printf("%02d %-30s %-10s %-15s (%s)\n", index+1, participant.Name, participant.Unit, participant.Username, participant.Initials)
-		qrcode += fmt.Sprintln(participant.Name)
-	}
-
-	printQRcode(qrcode)
-
-}
-
-func printQRcode(s string) {
-	qrterminal.Generate(s, qrterminal.L, os.Stdout)
+// PrettyPrint prints the Participant prettily to stdOut
+func (p *Participant) PrettyPrint(index int) {
+	fmt.Printf(
+		"%02d %-30s %-10s %-15s (%s)\n",
+		index+1,
+		p.Name,
+		p.Unit,
+		p.Username,
+		p.Initials,
+	)
 }
